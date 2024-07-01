9a0f85bb531e92275daf27ee3660e35d66a3db16
cacayoww <maritza.amalia.dwiputri@gmail.com>
2024-07-01 14:24:59 +0700

    update again

--- resources/views/layout/components/menu.blade.php
+++ resources/views/layout/components/menu.blade.php
@@ -1,4 +1,3 @@
-
 <ul class="nav nav-underline nav-fill nav-justified px-3 " style="--bs-nav-underline-gap: 0rem;">
     <li class="nav-item">
         <a class="nav-link active" aria-current="page" href="#">Active</a>
@@ -26,7 +25,3 @@
     </li>
 
 </ul>
-
-<div class="px-3">
-    <hr style="margin: 0px;" />
-</div>
--- resources/views/layout/components/profile.blade.php
+++ resources/views/layout/components/profile.blade.php
@@ -1,17 +1,18 @@
-<div class="d-flex px-3 pt-3">
+<div class="d-flex px-3 pt-3" style="flex-direction: column">
     <div class="row w-100">
-        <div class="col-2 d-flex justify-content-start">
-            <img src="img/user.jpg" alt="..." class="rounded-circle">
-        </div>
-        <div class="col-4 d-flex justify-content-start align-items-start"
-            style="flex-direction: column;">
-            <p>Nama/NIK</p>
-            <p style="margin: 1px">....</p>
-            <p style="margin: 1px">....</p>
-            <p style="margin: 1px">....</p>
+        <div class="col d-flex" style="flex=direction: row">
+            <div class="col-4 d-flex justify-content-start">
+                <img src="img/user.jpg" alt="..." class="rounded-circle">
+            </div>
+            <div class="col-8 d-flex justify-content-start align-items-start" style="flex-direction: column;">
+                <p>Nama/NIK</p>
+                <p style="margin: 1px">....</p>
+                <p style="margin: 1px">....</p>
+                <p style="margin: 1px">....</p>
 
+            </div>
         </div>
-        <div class="col d-flex justify-content-start align-items-end">
+        <div class="col d-lg-flex d-none justify-content-start align-items-end">
             <div class="row w-100">
                 <div class="col-3">
                     <button type="button" class="btn btn-primary rounded-pill m-2">Primary</button>
@@ -32,5 +33,27 @@
             </div>
         </div>
 
+    </div>
+    <div class="row w-100">
+        <div class="col d-lg-none d-flex justify-content-start align-items-end">
+            <div class="row w-100">
+                <div class="col-3">
+                    <button type="button" class="btn btn-primary rounded-pill m-2">Primary</button>
+                </div>
+                <div class="col-3">
+                    <button type="button" class="btn btn-primary rounded-pill m-2">Primary</button>
+
+                </div>
+                <div class="col">
+                    <div class="input-group">
+                        <input type="text" class="form-control">
+                        <!-- <div class="input-group-append">
+                            <span class="input-group-text">.00</span>
+                        </div> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </div>
